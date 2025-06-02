b173e754d2b917043b241713d42f35f13cf38252
Josseline <josseline.coutino29@unach.mx>
2025-06-01 21:41:23 -0600

    prueba psicologos

--- Proyecto_Mujeres-master/proyecto_unach/alumnas/vista/psicologos.php
+++ Proyecto_Mujeres-master/proyecto_unach/alumnas/vista/psicologos.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../vista/STATIC/CSS/psico.css"> 
     <title>Psicología</title> 
 </head>
 <body>
     <header class="encabezado">
-        <div class="logo-psi">
-            <img src="../vista/img/empatia.png" alt="logo psicología">
-            <h3 class="titulo">Tienes el poder de crear<br>lo que te propongas.</h3>
+        <div class="logo-psi"> 
+            <img src="../vista/img/empatia.png" alt="logo psicología" class="logo-size"> 
+            <h3 class="titulo">
+                Tienes el poder de crear<br>
+                <span style="color: #EF8118;">lo que te propongas.</span>  
+            </h3>
 
             <div class="cont-navbar">
                 <nav>
@@ -23,7 +27,39 @@
         </div>
     </header>
 
-    
+    <div class="container">
+        <section class="hero">
+            <div class="hero-content">
+                <div class="decorative-line-container">
+                    <div class="line-segment blue"></div>
+                    <div class="line-segment black"></div>
+                </div>
+                <h1>Prioritize Your<br><span class="highlight-blue">Mental Health.</span></h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore.</p>
+                <a href="#" class="btn btn-secondary">Learn More →</a>
+            </div>
+            <div class="hero-image-section">
+                <div class="dots-pattern top-dots">
+                    <span></span><span></span><span></span>
+                    <span></span><span></span><span></span>
+                    <span></span><span></span><span></span>
+                </div>
+                <img src="../vista/img/Container-psico.jpg" alt="Desk with brain model, keyboard, glasses, and notebook" class="hero-image">
+                <div class="dots-pattern bottom-dots">
+                    <span></span><span></span><span></span>
+                    <span></span><span></span><span></span>
+                    <span></span><span></span><span></span>
+                </div>
+            </div>
+        </section>
+        <section class="mood-tracker">
+            <div class="mood-item">😊</div>
+            <div class="mood-item dark">😟</div>
+            <div class="mood-item">😊</div>
+            <div class="mood-item dark">😟</div>
+        </section>
+    </div>
+
     
     
 </body>
